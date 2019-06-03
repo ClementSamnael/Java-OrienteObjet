4126199c2c551ee6f9314689c442992d135e130f
Clement ORMAUX <clement.ormaux@gmail.com>
2019-06-03 14:50:14 +0200

    exercice

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		Cartes carte1 = new Cartes(1, "Pique");
-		Cartes carte2 = new Cartes(12, "Carreau");
-		Cartes carte3 = new Cartes(8, "Coeur");
+		Cartes carte2 = new Cartes(8, "Carreau");
+		Cartes carte3 = new Cartes(12, "Coeur");
 
 		System.out.println(carte1);
 		System.out.println(carte2);

65d134c016c2dfdbf90a536ce1a7a1a70aeed9eb
Clement ORMAUX <clement.ormaux@gmail.com>
2019-06-03 15:08:59 +0200

    exemple Instance

--- src/main/java/exemple/Main.java
+++ src/main/java/exemple/Main.java
@@ -3,14 +3,14 @@ package exemple;
 public class Main {
 
 	public static void main(String[] args) {
-		Personne p1 = new Personne("Mattieu");
-		Personne p2 = new Personne("Mathilde");
-		Personne p3 = new Personne("Robin");
+		Personne p1 = new Personne();
+		Personne p2 = new Personne();
+//		Personne p3 = new Personne("Robin");
 
 		p2.setName("Jean Baptiste");
-		System.out.println(p1.getName());
-		System.out.println(p2.getName());
-		System.out.println(p3.getName());
+		System.out.println(p1.nbInstance);
+		System.out.println(p2.nbInstance);
+		System.out.println(Personne.nbInstance);
 	}
 
 }
--- src/main/java/exemple/Personne.java
+++ src/main/java/exemple/Personne.java
@@ -4,13 +4,16 @@ public class Personne {
 
 	private String name;
 
+	public static int nbInstance = 0;
+
 	public Personne(String name) {
 		this.name = name;
 	}
 
 	// Constructeur par défaut (implicite) sans paramètre
-	// public Personne() {
-	// }
+	public Personne() {
+		nbInstance++;
+	}
 
 	public String getName() {
 		return name;
@@ -21,11 +24,11 @@ public class Personne {
 	}
 
 	// Personne p => paramètre donc this.comparer(paramètre)
-	public void comparer(Personne p) {
-		if (p.name.equals(this.name)) {
-			System.out.println("Homonyme !");
-		} else {
-			System.out.println("Différent !");
-		}
-	}
+//	public void comparer(Personne p) {
+//		if (p.name.equals(this.name)) {
+//			System.out.println("Homonyme !");
+//		} else {
+//			System.out.println("Différent !");
+//		}
+//	}
 }

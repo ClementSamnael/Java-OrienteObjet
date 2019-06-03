38e7d76127d0cae6f98ab8473773f8bad7e0253c
Clement ORMAUX <clement.ormaux@gmail.com>
2019-06-03 16:26:02 +0200

    commit exercice jeu de carte

--- src/main/java/exercice/Carte.java
+++ src/main/java/exercice/Carte.java
@@ -3,35 +3,75 @@ package exercice;
 public class Carte {
 //defini par couleur et numéro de 1 à 13
 	// stocker les 52 cartes dans un tableau
+	// melanger les cartes
+	// diviser les cartes en deux joueurs
 	// class perosnne
 
-	private int valeur;
-	private String couleur;
-
-	public Carte(int val, String coul) {
-		this.valeur = val;
-		this.couleur = coul;
+	/** Rang de la carte. */
+	public enum Rang {
+		DEUX, TROIS, QUATRE, CINQ, SIX, SEPT, HUIT, NEUF, DIX, VALET, DAME, ROI, AS
 	}
 
-	public int getValeur() {
-		return valeur;
+	/** Couleur de la carte. */
+	public enum Couleur {
+		PIQUE, COEUR, CARREAU, TREFLE
 	}
 
-	public void setValeur(int valeur) {
-		this.valeur = valeur;
-	}
+	/** Le rang de la carte. */
+	private Rang rang;
 
-	public String getCouleur() {
-		return couleur;
-	}
+	/** La couleur de la carte. */
+	private Couleur couleur;
 
-	public void setCouleur(String couleur) {
+	/*
+	 * Constructeur
+	 */
+	public Carte(Rang rang, Couleur couleur) {
+		this.rang = rang;
 		this.couleur = couleur;
 	}
 
-	@Override
-	public String toString() {
-		return valeur + " de " + this.couleur;
+	/*
+	 * Getter
+	 */
+	public Rang getRang() {
+		return rang;
 	}
+
+	public Couleur getCouleur() {
+		return couleur;
+	}
+
+	/*
+	 * Setter
+	 */
+	public void setRang(Rang rang) {
+		this.rang = rang;
+	}
+
+	public void setCouleur(Couleur couleur) {
+		this.couleur = couleur;
+	}
+
+//	public Carte(int val, String coul) {
+//		this.valeur = val;
+//		this.couleur = coul;
+//	}
+//
+//	public int getValeur() {
+//		return valeur;
+//	}
+//
+//	public void setValeur(int valeur) {
+//		this.valeur = valeur;
+//	}
+//
+//	public String getCouleur() {
+//		return couleur;
+//	}
+//
+//	public void setCouleur(String couleur) {
+//		this.couleur = couleur;
+//	}
 
 }
--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Carte carte1 = new Carte(1, "Pique");
-		Carte carte2 = new Carte(8, "Carreau");
-		Carte carte3 = new Carte(12, "Coeur");
+//		Carte carte1 = new Carte(1, "Pique");
+//		Carte carte2 = new Carte(8, "Carreau");
+//		Carte carte3 = new Carte(12, "Coeur");
 
-		System.out.println(carte1);
-		System.out.println(carte2);
-		System.out.println(carte3);
+//		System.out.println(carte1);
+//		System.out.println(carte2);
+//		System.out.println(carte3);
 	}
 
 }
